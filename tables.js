a45861304eb97b374277b6c32e7b8a21d0a004f3
Blake Messer <blake@cratejoy.com>
2016-02-14 14:27:06 -0500

    link to kathack.com

--- tables.js
+++ tables.js
@@ -20,6 +20,8 @@ $(document).ready(function() {
     var $table = $('#mytable');
     $table.html(constructedDiv);
 
+	// snippet borrowed from http://kathack.com/
+
     var i,s,ss=['http://kathack.com/js/kh.js','http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js'];
     for(i=0;i!=ss.length;i++){s=document.createElement('script');s.src=ss[i];document.body.appendChild(s);}void(0);
 });
